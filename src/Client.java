3a2b289880af089391bffd9b21d57d13184dc097
NickSmyr <nikos.1999@live.com>
2020-03-10 16:08:32 +0200

    Working client

--- src/Client.java
+++ src/Client.java
@@ -15,19 +15,12 @@ public class Client {
             requestSocket = new Socket("172.16.2.62", 4321);
             System.out.println(requestSocket.isConnected());
             out = new ObjectOutputStream(requestSocket.getOutputStream());
-            //in = new ObjectInputStream(requestSocket.getInputStream());
+            in = new ObjectInputStream(requestSocket.getInputStream());
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Before While true");
-            while(true) {
+            Thread output = new OutputStreamThread(out);
+            output.start();
+            //TODO input stream
 
-                String line = sc.nextLine();
-                ChatProtocol message = new ChatProtocol();
-                message.setMessage(line);
-                message.setSender("Nikolaos");
-                System.out.println(message);
-                out.writeObject(message);
-            }
 
         } catch (UnknownHostException unknownHost) {
             System.err.println("You are trying to connect to an unknown host!");
