2db83743d2824a99ab8e775ee1a7ea7a13869ba4
NickSmyr <nikos.1999@live.com>
2020-03-10 15:31:26 +0200

    Chat Protocol , Client

---
+++ src/Client.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public void run() {
+        Socket requestSocket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
+        try {
+
+            requestSocket = new Socket("127.0.0.1", 4321);
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            in = new ObjectInputStream(requestSocket.getInputStream());
+            /*
+             *
+             *
+             *
+             */
+            Scanner sc = new Scanner(System.in);
+
+        } catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host!");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                in.close();	out.close();
+                requestSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+}
