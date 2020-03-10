a7d3bd551cfc9e017e6be8137aeae103ab52c148
xrisaD <xrdikonimaki@hotmail.com>
2020-03-10 15:56:23 +0200

    client to server send message

--- src/Server.java
+++ src/Server.java
@@ -10,23 +10,35 @@ public class Server {
     ServerSocket providerSocket;
     Socket connection = null;
 
+    //ObjectOutputStream out = null;
+
     void openServer() {
+        ObjectInputStream in = null;
+        ChatProtocol e = null;
         try {
 
             providerSocket = new ServerSocket(4321, 10);
 
             while (true) {
                 connection = providerSocket.accept();
-                /*
-                 *
-                 *
-                 *
-                 */
+                in = new ObjectInputStream(connection.getInputStream());
+
+                //out = new ObjectOutputStream(connection.getOutputStream());
+                while(true) {
+                    e = (ChatProtocol) in.readObject();
+                    System.out.println(e);
+                }
+                //out.writeObject();
+               // out.close();
             }
 
         } catch (IOException ioException) {
             ioException.printStackTrace();
-        } finally {
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
+        }finally {
             try {
                 providerSocket.close();
             } catch (IOException ioException) {
